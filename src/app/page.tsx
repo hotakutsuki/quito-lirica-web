0a2176c81569f9bfb2b9281f1d2fa2f41826ccb1
Josue Ortiz <jotakutsuki@gmail.com>
2025-09-10 14:29:37 +0000

    agreguemos un pequeno padding horizontal a toda la pagina. algunos texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import ContactSection from '@/components/sections/contact';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
+    <div className="flex min-h-screen flex-col bg-background px-4">
       <Header />
       <main className="flex-1">
         <HeroSection />
