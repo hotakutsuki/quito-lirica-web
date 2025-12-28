a7a9dda19e3c328a2b8ebeb9384173e5e5afee7a
Josue Ortiz <jotakutsuki@gmail.com>
2025-12-28 16:15:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "quito-lirica",
+  "appId": "1:493144957121:web:c97d4b704c0153cbd2f440",
+  "apiKey": "AIzaSyAvyTbN5vOwIZ5IrRNiZjwcTpsGzA0bdFU",
+  "authDomain": "quito-lirica.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "493144957121"
+};
