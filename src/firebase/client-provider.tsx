8a503adce7105e706e9928456f80daafb562ea48
Josue Ortiz <jotakutsuki@gmail.com>
2025-12-28 16:36:11 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useMemo, type ReactNode } from 'react';
+import React, { type ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
 import { initializeFirebase } from '@/firebase';
 
@@ -8,11 +8,10 @@ interface FirebaseClientProviderProps {
   children: ReactNode;
 }
 
+// Initialize Firebase on the client side, only once per application load.
+const firebaseServices = initializeFirebase();
+
 export function FirebaseClientProvider({ children }: FirebaseClientProviderProps) {
-  const firebaseServices = useMemo(() => {
-    // Initialize Firebase on the client side, once per component mount.
-    return initializeFirebase();
-  }, []); // Empty dependency array ensures this runs only once on mount
 
   return (
     <FirebaseProvider
